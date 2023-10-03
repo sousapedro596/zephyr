d2d84e94b9487e0003b4835910eb2ed1022a6de0
Pedro Sousa <sousapedro596@gmail.com>
2023-10-04 00:40:25 +0100

    sample: sample to trigger race condition in k_timer_start
    Changes the hello_world sample to have two timers, one to
    interrupt the execution of main and the second to always be
    resechduling from main. Quite fast this triggers the
    assertion in z_add_timeout. Tested in qemu_x86_64
    
    Signed-off-by: Pedro Sousa <sousapedro596@gmail.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -1,13 +1,33 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <stdio.h>
+#include <zephyr/kernel.h>
 
-int main(void)
+void handler_sim_isr(struct k_timer *timer);
+
+K_TIMER_DEFINE(sim_isr, handler_sim_isr, NULL);
+K_TIMER_DEFINE(timer_to_be_interrupted, NULL, NULL);
+
+k_timeout_t timeout_main = K_MSEC(100);
+
+/* ISR timer handler to simulate HW interrupt */
+void handler_sim_isr(struct k_timer *timer)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD);
-	return 0;
+	/* reconfigure the timer under test, eventually we will interrupt 
+	  while k_timer_start already started on main  to trigger the assert
+	*/
+	k_timer_start(&timer_to_be_interrupted, K_MSEC(1000), K_MSEC(1000));
+
+	printk("simulating an interrupt\n");
+
+}
+
+
+int main()
+{
+	/* timer to simulate an interrupt */
+	k_timer_start(&sim_isr, K_USEC(1000), K_USEC(1000));
+
+	while(1) {
+		k_timer_start(&timer_to_be_interrupted, K_MSEC(100), K_MSEC(100));
+	}
+
 }
